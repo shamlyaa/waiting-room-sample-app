8152c25a99a5e404ce9b7d9e08a613ceb754879d
javiermolsanz <javier.molinasanz@vonage.com>
2021-07-02 14:17:30 +0100

    resolving subcribe issue

--- public/index.js
+++ public/index.js
@@ -2,9 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const roomName = urlParams.get('room');
 
 let connectionCount = 0;
-let session = null;
-let publisher = null;
-let isPublishing = false;
+// let isPublishing = false;
 let usersConnected = [];
 
 function handleError(error) {
@@ -13,98 +11,12 @@ function handleError(error) {
   }
 }
 
-function handlePublisher(pub) {
-  console.log('[handlePublish]', connectionCount);
-  if (!isPublishing) {
-    // todo publish twice the same stream explode everything
-    session.publish(pub, handleError);
-  } else if (connectionCount === 0 && publisher) {
-    session.unpublish(pub);
-  }
-}
-
-function initializeSession(data) {
-  const { apiKey, sessionId, token } = data;
-  session = OT.initSession(apiKey, sessionId);
-
-  session.on('connectionCreated', function(event) {
-    usersConnected.push(event.connection);
-    console.log(usersConnected);
-    console.log('[connectionCreated]', connectionCount);
-
-    console.log(isHostPresent());
-
-    connectionCount += 1;
-    handlePublisher(publisher);
-  });
-
-  session.on('connectionDestroyed', function(event) {
-    console.log('[connectionDestroyed]', connectionCount);
-    usersConnected.filter(connection => connection.id != event.connection.id);
-    console.log(usersConnected);
-    connectionCount -= 1;
-    handlePublisher(publisher);
-  });
-
-  // Subscribe to a newly created stream
-  session.on('streamCreated', function streamCreated(event) {
-    const subscriberOptions = {
-      insertMode: 'append',
-      width: '60%',
-      height: '60%'
-    };
-    session.subscribe(
-      event.stream,
-      'subscribers',
-      subscriberOptions,
-      handleError
-    );
-  });
-
-  session.on('sessionDisconnected', function sessionDisconnected(event) {
-    console.log('You were disconnected from the session.', event.reason);
-  });
-
-  // initialize the publisher
-  var publisherOptions = {
-    insertMode: 'append',
-    width: '100%',
-    height: '100%'
-  };
-  publisher = OT.initPublisher('publisher', publisherOptions, handleError);
-  publisher.on('streamCreated', event => {
-    console.log('[Publisher] - streamCreated', event);
-    isPublishing = true;
-  });
-  publisher.on('streamDestroyed', event => {
-    console.log('[Publisher] - streamDestroyed', event.reason);
-    event.preventDefault();
-    isPublishing = false;
-  });
-
-  // Connect to the session
-  session.connect(token, function callback(error) {
-    if (error) {
-      handleError(error);
-    } else {
-      console.log('Session Connected');
-    }
-  });
-}
-
 const isHostPresent = () => {
   if (usersConnected.find(e => e.data === 'admin')) {
     return true;
   } else {
     return false;
   }
-};
-
-const disconnectAll = () => {
-  usersConnected.map(connection => {
-    console.log('disconnecting ' + connection);
-    session.forceDisconnect(connection);
-  });
 };
 
 // initializeSession();
